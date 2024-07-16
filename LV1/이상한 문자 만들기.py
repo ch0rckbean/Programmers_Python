be37b1a305e0e19cc594d7ef35c72073f0006d0d
Bean <86273626+ch0rckbean@users.noreply.github.com>
2024-07-17 01:05:43 +0900

    study: 이상한 문자 만들기

--- LV1/이상한 문자 만들기.py
+++ LV1/이상한 문자 만들기.py
@@ -1,13 +1,17 @@
-def solution(stc):
+def solution(str):
     answer = ''
-    print(stc.split(' ')) # 배열 반환 ['try', 'hello', 'world']
-    
-    for i in stc:
-           for j in i:
-    
-    print(answer)
-   
-    return answer
+    arr = str.split('')
+    # split()는 공백 여러개여도 하나로 처리
+    # split('')는 공백 여러개면 여러개 그대로 처리
+
+    for i in arr:  # 각 단어: try, hello, world
+        for j in range(len(i)):  # 각 알파벳의 철자 수: 3, 5, 5
+            if j % 2 == 0:  # 짝수일 때
+                answer += i[j].upper()
+            else:
+                answer += i[j].lower()
+        answer += ' '  # 한 단어가 돌 때마다 공백 추가
+    return answer[:-1]  # 마지막 공백 제거
 
 
 solution('try hello world')
