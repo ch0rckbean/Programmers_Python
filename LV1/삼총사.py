be8813fea7d98407b207ff45757d3ec9cfe14384
Bean <86273626+ch0rckbean@users.noreply.github.com>
2024-07-17 18:39:45 +0900

    study: 삼총사

---
+++ LV1/삼총사.py
@@ -0,0 +1,15 @@
+from itertools import combinations
+
+
+def solution(number):
+    answer = 0
+    for i in combinations(number, 3):
+        if sum(i) == 0:
+            answer += 1
+
+    print(answer)
+    return answer
+
+
+solution([-2, 3, 0, 2, -5])
+solution([-1, 1, -1, 1])
