ac970f99f5483f413ebd030eb64a99695a8de565
Bean <86273626+ch0rckbean@users.noreply.github.com>
2024-07-20 17:40:39 +0900

    study: 숫자 문자열과 영단어

---
+++ LV1/숫자 문자열과 영단어.py
@@ -0,0 +1,16 @@
+def solution(s):
+    answer = 0
+    dic = {'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4',
+           'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
+
+    for i in dic.keys():
+        if i in s:
+            s = s.replace(i, dic[i])
+    answer = int(s)
+
+    print(answer)
+    return answer
+
+
+solution('one4seveneight')
+solution('23four5six7')
