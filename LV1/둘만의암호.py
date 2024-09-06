2d467918ee21e9b9a8524dff5a3a61184b502267
Bean <86273626+ch0rckbean@users.noreply.github.com>
2024-09-06 13:33:21 +0900

    study: 둘만의 암호

---
+++ LV1/둘만의암호.py
@@ -0,0 +1,20 @@
+import string
+
+
+def solution(s, skip, index):
+    lower = [i for i in string.ascii_lowercase]
+    lower = [ch for ch in lower if ch not in skip]  # skip의 문자 제거
+    answer = ''
+    s = list(s)
+
+    for i in range(len(s)):
+        idx = lower.index(s[i])
+        # 각 문자를 인덱스만큼 민 알파벳 매치 : 나머지를 잘 활용하자
+        s[i] = lower[(idx+index) % len(lower)]
+        answer += s[i]
+    print(answer, s)
+
+    return answer
+
+
+solution("aukks", "wbqd", 5)
